6ade5c5eb217eb4af1b66ef9d9905d46a3c98eab
Christopher Marinica <chris.marinica@gmail.com>
2022-11-01 13:15:56 -0400

    commit tree 8c6d31141cd2705e4d40b22e91bac4d72c27676b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: "Chris",
+    streetAddress: "Ellen Dr",
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    return {
+      ...employee,
+      [name]: streetAddress,
+    };
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name] = streetAddress; 
+  
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, name, streetAddress) {
+    return {
+      ...employee,
+      [name]: streetAddress,
+    };
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, name, streetAddress) {
+    employee[name] = streetAddress; 
+return employee  
+}
